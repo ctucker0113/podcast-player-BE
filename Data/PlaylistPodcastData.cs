3bae659f49efd57ec8b05f476342e415aee88691
Lucas Martin <lucasmartin11990@gmail.com>
2024-04-22 20:48:17 -0500

    finishing up mock data

--- Data/PlaylistPodcastData.cs
+++ Data/PlaylistPodcastData.cs
@@ -9,9 +9,13 @@ namespace podcast_player_BE.Data
             new PlaylistPodcast { Id = "1", PlaylistId = "1", PodcastId = "1" },
             new PlaylistPodcast { Id = "2", PlaylistId = "1", PodcastId = "2" },
             new PlaylistPodcast { Id = "3", PlaylistId = "1", PodcastId = "3" },
-            new PlaylistPodcast { Id = "4", PlaylistId = "2", PodcastId = "2" },
-            new PlaylistPodcast { Id = "5", PlaylistId = "2", PodcastId = "3" },
-            new PlaylistPodcast { Id = "6", PlaylistId = "3", PodcastId = "3" }
+            new PlaylistPodcast { Id = "4", PlaylistId = "2", PodcastId = "4" },
+            new PlaylistPodcast { Id = "5", PlaylistId = "2", PodcastId = "5" },
+            new PlaylistPodcast { Id = "6", PlaylistId = "3", PodcastId = "6" },
+            new PlaylistPodcast { Id = "7", PlaylistId = "4", PodcastId = "7" },
+            new PlaylistPodcast { Id = "8", PlaylistId = "4", PodcastId = "8" },
+            new PlaylistPodcast { Id = "9", PlaylistId = "5", PodcastId = "9" },
+            new PlaylistPodcast { Id = "10", PlaylistId = "5", PodcastId = "10" }
         };
     }
 }
