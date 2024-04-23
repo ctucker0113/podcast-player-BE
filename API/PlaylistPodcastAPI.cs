793d93f0f501993e027a6dcd6170d3a4448cc394
Lucas Martin <lucasmartin11990@gmail.com>
2024-04-22 20:24:46 -0500

    just doing setup and file structure

---
+++ API/PlaylistPodcastAPI.cs
@@ -0,0 +1,14 @@
+﻿using podcast_player_BE.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace podcast_player_BE.API
+{
+    public static class PlaylistPodcastAPI
+    {
+        public static void Map(WebApplication app)
+        {
+        }
+    }
+}
